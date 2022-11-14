faa1d8742d6ba1992a62c3d706109c54e0d45c41
Albasits <albasitseptian94@gmail.com>
2022-11-14 21:00:15 +0700

    menambahkan fitur add contact dan validator input format nama, email dan nohp

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require(`express`);
 const expressLayouts = require(`express-ejs-layouts`);
+
+const { body, validationResult, check } = require(`express-validator`);
+
 const session = require(`express-session`);
 const cookieParser = require(`cookie-parser`);
 const flash = require(`connect-flash`);
@@ -68,6 +71,46 @@ app.get(`/contact`, async (req, res) => {
     msg: req.flash(`msg`),
   });
 });
+
+// halaman form tambah contact
+app.get(`/contact/add`, (req, res) => {
+  res.render(`add-contact`, {
+    title: `Add New Contact Page`,
+    layout: `layout/mainLayout`,
+  });
+});
+
+// Proses tambah data contact
+app.post(
+  `/contact`,
+  [
+    body(`nama`).custom(async (value) => {
+      const duplikat = await Contact.findOne({ nama: value });
+      if (duplikat) {
+        throw new Error(`Name Already Registered!`);
+      }
+      return true;
+    }),
+    check(`email`, `Invalid Email Format!`).isEmail(),
+    check(`nohp`, `Invalid Phone Number (id-ID)!`).isMobilePhone(`id-ID`),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render(`add-contact`, {
+        title: `Form Add Contact`,
+        layout: `layout/mainLayout`,
+        errors: errors.array(),
+      });
+    } else {
+      Contact.insertMany(req.body, (error, result) => {
+        // kirimkan flash message
+        req.flash(`msg`, `contact added successfully!`);
+        res.redirect(`/contact`);
+      });
+    }
+  }
+);
 
 // halaman detail contact
 app.get(`/contact/:nama`, async (req, res) => {
