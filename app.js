b17ca88276df31f7330bfbbcddd24946500e2c6c
Albasits <albasitseptian94@gmail.com>
2022-11-15 14:48:00 +0700

    menambahkan fitur delete

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const expressLayouts = require(`express-ejs-layouts`);
 
 const { body, validationResult, check } = require(`express-validator`);
 
+const methodOverride = require(`method-override`);
+
 const session = require(`express-session`);
 const cookieParser = require(`cookie-parser`);
 const flash = require(`connect-flash`);
@@ -12,6 +14,9 @@ const Contact = require(`./model/contact`);
 
 const app = express();
 const port = 3000;
+
+// setup method override
+app.use(methodOverride(`_method`));
 
 // Setup EJS
 app.set(`view engine`, `ejs`);
@@ -108,6 +113,74 @@ app.post(
         req.flash(`msg`, `contact added successfully!`);
         res.redirect(`/contact`);
       });
+    }
+  }
+);
+
+// proses delete contact
+// app.get(`/contact/delete/:nama`, async (req, res) => {
+//   const contact = await Contact.findOne({ nama: req.params.nama });
+
+//   // jika contact tidak ada
+//   if (!contact) {
+//     res.status(404);
+//     res.send(`<h1>404 Not found</h1>`);
+//   } else {
+//     // Contact.deleteOne({ nama: req.params.nama }); //untuk menghapus berdasarkan nama
+//     Contact.deleteOne({ _id: contact._id }).then((result) => {
+//       req.flash(`msg`, `contact deleted successfully!`);
+//       res.redirect(`/contact`);
+//     });
+//   }
+// });
+
+app.delete(`/contact`, (req, res) => {
+  Contact.deleteOne({ nama: req.body.nama }).then((result) => {
+    req.flash(`msg`, `contact deleted successfully!`);
+    res.redirect(`/contact`);
+  });
+});
+
+// halaman form ubah/edit data contact
+app.get(`/contact/edit/:nama`, async (req, res) => {
+  const contact = await Contact.findOne({ nama: req.params.nama });
+
+  res.render(`edit-contact`, {
+    title: `Edit Contact Page`,
+    layout: `layout/mainLayout`,
+    contact,
+  });
+});
+
+// proses update data contact
+app.put(
+  `/contact`,
+  [
+    body(`nama`).custom((value, { req }) => {
+      const duplikat = cekDuplikat(value);
+      if (value !== req.body.oldNama && duplikat) {
+        throw new Error(`Name Already Registered!`);
+      }
+      return true;
+    }),
+    check(`email`, `Invalid Email Format!`).isEmail(),
+    check(`nohp`, `Invalid Phone Number (id-ID)!`).isMobilePhone(`id-ID`),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // return res.status(400).json({ errors: errors.array() });
+      res.render(`edit-contact`, {
+        title: `Form Edit Contact`,
+        layout: `layout/mainLayout`,
+        errors: errors.array(),
+        contact: req.body,
+      });
+    } else {
+      updateContacts(req.body);
+      // kirimkan flash message
+      req.flash(`msg`, `contact edited successfully!`);
+      res.redirect(`/contact`);
     }
   }
 );
